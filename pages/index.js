f27b69ec3bb03dafc2abe7f382d0601c6fd88cb3
Edes Sulce <edessulce@outlook.com>
2021-11-10 23:58:43 +0100

    AS

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,13 @@ import Images from '../components/Image'
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>Creo</title>
+        <meta name="description" content="Creo - Digital Agency" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#000" />
+        {/* <link rel="icon" href="/favicon.ico" /> */}
+      </Head>
       <Header />
       <section className="home-hyrja container flex jc-center ai-center" >
         <Images src={HyrjaImage} alt="Colorful globe" />
