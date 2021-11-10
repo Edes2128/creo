a4089a4b80b7f94f5024b5da77f233ecf0906d75
Edes Sulce <edessulce@outlook.com>
2021-11-10 23:28:43 +0100

    SAS

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,10 +2,17 @@ import React from 'react'
 import Images from './Image'
 import Logo from '../images/logo.png'
 
+
 export default function Header() {
     return (
         <header className="header container flex jc-spaceb ai-center" >
             <Images src={Logo} alt="Logo" />
+            <svg style={{cursor:"pointer"}} xmlns="http://www.w3.org/2000/svg" width="34" height="10" viewBox="0 0 34 10">
+                <g id="Group_21" data-name="Group 21" transform="translate(-1738.89 -58)">
+                    <rect id="Rectangle_3" data-name="Rectangle 3" width="34" height="2" transform="translate(1738.89 58)" fill="#fff" />
+                    <rect id="Rectangle_35" data-name="Rectangle 35" width="34" height="2" transform="translate(1738.89 66)" fill="#fff" />
+                </g>
+            </svg>
         </header>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,15 @@
 import Head from 'next/head'
 import Header from '../components/Header'
+import HyrjaImage from '../images/glob_home.png'
+import Images from '../components/Image'
 
 export default function Home() {
   return (
     <>
       <Header />
+      <section className="home-hyrja container flex jc-center ai-center" >
+        <Images src={HyrjaImage} alt="Colorful globe" />
+      </section>
     </>
   )
 }
