0441d229b472fa45f9c117802dc66c4bf5a1d0be
Edes Sulce <edessulce@outlook.com>
2021-11-13 11:02:48 +0100

    ASS

--- pages/about/components/AboutServices.jsx
+++ pages/about/components/AboutServices.jsx
@@ -1,9 +1,52 @@
-
-
 export default function AboutServices() {
     return (
         <section className="about-services" >
+            <div className="about-services-container container" >
+                <p className="title fw-medium" >Our services</p>
+                <div className="items flex ai-start jc-spaceb">
 
+                    <div className="item">
+                        <p className="title fw-bold">
+                            A passionate team with a clear goal when releasing <br />
+                            new digital products. We want to universalize our <br />
+                            clients presence no matter the channel or device. <br />
+                        </p>
+                        <p className="text fw-regular" >
+                            We work in small teams to deliver cost-effective, results-driven <br />
+                            solutions that offer a personalised service with <br />
+                            quantifiable results for our clients.<br />
+                        </p>
+                    </div>
+                    <div className="item">
+                        <p className="title fw-bold">Implementation</p>
+                        <p className="text fw-regular">  Film & Photo Studio</p>
+                        <p className="text fw-regular">  Print Studio</p>
+                        <p className="text fw-regular">  Post-Production</p>
+                        <p className="text">Motion Design</p>
+
+                    </div>
+                    <div className="item">
+                        <p className="title fw-bold">Digital</p>
+                        <p className="text fw-regular">Website</p>
+                        <p className="text fw-regular">Responsive Design</p>
+                        <p className="text fw-regular">eCommerce Platforms</p>
+                        <p className="text fw-regular">Web & App Development</p>
+                        <p className="text fw-regular">CRM & CSM System</p>
+                        <p className="text fw-regular">Functional Prototyping</p>
+                        <p className="text fw-regular">SEO Strategy & Systems</p>
+                        <p className="text fw-regular">UX & UI Design</p>
+                    </div>
+                    <div className="item">
+                        <p className="title fw-bold">Branding</p>
+                        <p className="text fw-regular">Visual Identity & Naming</p>
+                        <p className="text fw-regular">Publication Design</p>
+                        <p className="text fw-regular">Packaging</p>
+                        <p className="text fw-regular">Posters</p>
+                        <p className="text fw-regular">Guidelines & Systems</p>
+                    </div>
+
+                </div>
+            </div>
         </section>
     )
 }
