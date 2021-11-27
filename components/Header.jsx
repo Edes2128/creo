2cd56eb302f71f75d7533e99c076e5f06d1ab430
Edes Sulce <guerrilsi46@gmail.com>
2021-11-27 14:27:22 +0100

    ASAS

--- components/Header.jsx
+++ components/Header.jsx
@@ -6,11 +6,10 @@ import Link from 'next/link'
 export default function Header() {
     return (
         <header className="header container flex jc-spaceb ai-center" >
-            <Link href="/" >
-                <a >
+            <Link href="/">
+                <a>
                     <Images src={Logo} alt="Logo" />
                 </a>
-
             </Link>
             <svg style={{ cursor: "pointer" }} xmlns="http://www.w3.org/2000/svg" width="34" height="10" viewBox="0 0 34 10">
                 <g id="Group_21" data-name="Group 21" transform="translate(-1738.89 -58)">
